c2efaca417b7f7c54c4c313a384d88385349345d
unclebean <unclebean2013@gmail.com>
2017-08-14 21:36:40 +0800

    create project for record & replay

---
+++ README.md
@@ -0,0 +1,17 @@
+# Blaster
+
+**Blaster is a tool of record & replay REST API, build by typescript**
+
+## Dev
+
+Compile Blaster
+
+    npm run compile
+
+Run test
+
+    npm run test
+
+Link npm package to local
+
+    npm link
---
+++ src/main.ts
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+var pkg = require(__dirname+'/../package.json');
+import * as fs from 'fs';
+import * as minimist from 'minimist';
+import * as jsyaml from 'js-yaml';
+import * as _ from 'lodash';
+
+process.on('SIGINT', function () {
+  console.log('Got a sigint bye...');
+  process.exit(0);
+});
+
+export function blasterInfo(args){
+    const info = [];
+    if(args.help){
+        info.push("this is a record & replay tool");
+    }
+    if(args.version){
+        info.push(pkg.version);
+    }
+    return info;
+}
+
+export function startup(args){
+    try{
+        const _options = jsyaml.safeLoad(fs.readFileSync(args._[1], 'utf8'));
+    }catch(e){
+        console.log("Please follow readme to provide correct .yml file.");
+    }
+}
+
+export default function main(args){
+    _.forEach(blasterInfo(args), (info)=>console.log(info));
+    startup(args); 
+}
+
+main(minimist(process.argv.slice(2), {alias:{help:'h', version:'v', extions:'e'}}));
