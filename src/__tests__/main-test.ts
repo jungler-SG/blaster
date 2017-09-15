ce59d616de0e6a40dd87897276227d4c889570ad
Jeff <zhangzhuzhefu@gmail.com>
2017-09-15 15:28:58 +0800

    to make tslint happy

--- src/__tests__/main-test.ts
+++ src/__tests__/main-test.ts
@@ -1,15 +1,15 @@
-var pkg = require(__dirname+'/../../package.json');
-import { blasterInfo } from '../main';
-import { expect } from 'chai';
-import 'mocha';
+const pkg = require(__dirname + "/../../package.json");
+import { expect } from "chai";
+import "mocha";
+import { blasterInfo } from "../main";
 
-describe('blasterInfo', () => {
-  it('give help command then help information should be returned', () => {
-    const result = blasterInfo({"help":"h"});
-    expect(result[0]).to.equal('this is a record & replay tool');
+describe("blasterInfo", () => {
+  it("give help command then help information should be returned", () => {
+    const result = blasterInfo({help: "h"});
+    expect(result[0]).to.equal("this is a record & replay tool");
   });
-  it('give version command then blaster version should be returned', () => {
-    const result = blasterInfo({"version":"v"});
+  it("give version command then blaster version should be returned", () => {
+    const result = blasterInfo({version: "v"});
     expect(result[0]).to.equal(pkg.version);
   });
 });
