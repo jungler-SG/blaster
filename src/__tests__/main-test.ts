6ec89be924016687c1284eb253d7fd721c824c8f
Jeff <zhangzhuzhefu@gmail.com>
2017-09-18 12:55:43 +0800

    delete and ignore bin folder

--- src/__tests__/main-test.ts
+++ src/__tests__/main-test.ts
@@ -1,18 +1,19 @@
-import { expect } from "chai";
+import {expect} from "chai";
 import "mocha";
-import { blasterInfo } from "../main";
+import {blasterInfo} from "../main";
+
 const pkg = require(__dirname + "/../../package.json");
 
 describe("blasterInfo", () => {
 
-  it("give help command then help information should be returned", () => {
-    const result = blasterInfo({help: "h"});
-    expect(result[0]).to.equal("this is a record & replay tool");
-  });
+    it("give help command then help information should be returned", () => {
+        const result = blasterInfo({help: "h"});
+        expect(result[0]).to.equal("this is a record & replay tool");
+    });
 
-  it("give version command then blaster version should be returned", () => {
-    const result = blasterInfo({version: "v"});
-    expect(result[0]).to.equal(pkg.version);
-  });
+    it("give version command then blaster version should be returned", () => {
+        const result = blasterInfo({version: "v"});
+        expect(result[0]).to.equal(pkg.version);
+    });
 
 });
